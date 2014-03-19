331e58fafd1a35548c0938b06db4483242fe054c
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2014-03-19 15:24:33 -0300

    forgot this admin autodiscover in the urls.py... damm

--- sauron/sauron/urls.py
+++ sauron/sauron/urls.py
@@ -4,6 +4,8 @@ from django.views.generic import TemplateView
 from django.contrib import admin
 
 
+admin.autodiscover()
+
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
